dbef96681373db98ee97e72bedcf5cc2a090dab9
oatkiller <robert@oatlab.com>
2014-10-08 21:59:12 -0400

    Restructured calculator as a jQuery plugin and got my single test passing

--- tests/spec/calculator_test.js
+++ tests/spec/calculator_test.js
@@ -1,13 +1,22 @@
 describe("calculator", function() {
+	var element;
+
   beforeEach(function() {
+		element = $(calculatorHTML);
+		$(document.body).append(element);
   });
 
+	afterEach(function () {
+		element.remove();
+	});
+
 	it("should fire an event with role == 6 when the 6 button is pressed",function () {
-		var handler = jasmine.createSpy()
-		calculator.on("buttonPress",handler);
-		$(".button[role=\"6\"]").triggerHandler("click");
+		element.calculator();
+		var handler = jasmine.createSpy();
+		element.on("buttonPress",handler);
+		element.find(".button[data-role=\"6\"]").trigger("click");
 		expect(handler).toHaveBeenCalled();
-		expect(handler.callCount).toBe(1);
-		expect(handler.mostRecentCall.args[1].role).toBe(6);
+		expect(handler.calls.count()).toBe(1);
+		expect(handler.calls.argsFor(0)[1].role).toBe(6);
 	});
 });
